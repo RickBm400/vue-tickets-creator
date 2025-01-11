9a1b453715258a2001345443c039f14fcbd8ca0e
Ricardo Barrios <rjbm29@gmail.com>
2025-01-11 00:07:11 -0500

    feat: ticket template component

---
+++ src/components/Ticket.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="ticket__container">
+        <div class="ticket__container-left--card">
+            <div></div>
+            <div>
+                Ticket Template
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped lang="sass">
+.ticket__container
+    background-color: #FFFAEC
+    width: 900px
+    height: 300px
+    border-radius: 20px
+    padding: 16px
+    &-left--card
+        position: relative
+        background-color: #578E7E
+        height: 100%
+        width: 70%
+        border-radius: 8px
+        padding: 16px
+        font-size: 2rem
+        @include bebas-neue
+</style>
