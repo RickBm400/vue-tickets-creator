b3494433a91a2d36276099080b8c3671ca6fb13d
Ricardo Barrios <rjbm29@gmail.com>
2025-01-18 01:58:02 -0500

    feat: prime vue ui library

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 import App from './App.vue';
 import router from './router';
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import './sass/index.sass';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(PrimeVue).mount('#app');
