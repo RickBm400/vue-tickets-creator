9ca08e86b17f1f8ff2b0b15ed6473e8a43fb5ae4
Ricardo Barrios <rjbm29@gmail.com>
2025-05-21 20:02:44 -0500

    update left side bar component route selection

--- src/layouts/LeftSideBar.vue
+++ src/layouts/LeftSideBar.vue
@@ -1,12 +1,14 @@
 <script setup>
 // vue setup
 import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute, useRouter } from 'vue-router'
 import { generalMemberSBOpts } from './utils/sideBarLinks'
 // primevue components
 import Menu from 'primevue/menu'
 import { Listbox } from 'primevue'
 // component props
+const { name, path } = useRoute()
+const router = useRouter()
 const { rootClass } = defineProps({
     rootClass: {
         type: String,
@@ -14,12 +16,20 @@ const { rootClass } = defineProps({
     },
 })
 
+// redirect to selected route
+const changeRoute = () => router.push({ path: selectedPage.value.route })
+
 const dashboardPages = ref([...generalMemberSBOpts])
-const selectedPage = ref()
+const selectedPage = ref({ label: name, route: path })
 </script>
 <template>
     <aside class="left-side-var__container" :class="rootClass">
-        <Listbox :model="selectedPage" :options="dashboardPages" option-label="label"> </Listbox>
+        <Listbox
+            v-model="selectedPage"
+            :options="dashboardPages"
+            option-label="label"
+            @change="changeRoute()"
+        />
     </aside>
 </template>
 <style scoped lang="sass"></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,6 +9,13 @@ export const routes = [
                 path: '',
                 component: () => import('@/views/dashboard/Events/index.vue'),
                 meta: { requiresAuth: true },
+                name: 'Tickets',
+            },
+            {
+                path: '/events-calendar',
+                component: () => import('@/views/dashboard/Events/index.vue'),
+                meta: { requiresAuth: true },
+                name: 'Calendario',
             },
         ],
     },
