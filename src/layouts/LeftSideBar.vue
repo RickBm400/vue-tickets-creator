26c9ed1c3d8567e41197db431fc7f842cd4f58a6
Ricardo Barrios <rjbm29@gmail.com>
2025-05-24 18:14:23 -0500

    feat: implement logout and config button at sidebar

--- src/layouts/LeftSideBar.vue
+++ src/layouts/LeftSideBar.vue
@@ -5,7 +5,7 @@ import { RouterLink, useRoute, useRouter } from 'vue-router'
 import { generalMemberSBOpts } from './utils/sideBarLinks'
 // primevue components
 import Menu from 'primevue/menu'
-import { Listbox } from 'primevue'
+import { Listbox, Button } from 'primevue'
 // component props
 const { name, path } = useRoute()
 const router = useRouter()
@@ -16,6 +16,12 @@ const { rootClass } = defineProps({
     },
 })
 
+const buttons_pt_property = {
+    root: {
+        class: 'solid-border',
+    },
+}
+
 // redirect to selected route
 const changeRoute = () => router.push({ path: selectedPage.value.route })
 
@@ -23,13 +29,27 @@ const dashboardPages = ref([...generalMemberSBOpts])
 const selectedPage = ref({ label: name, route: path })
 </script>
 <template>
-    <aside class="left-side-var__container" :class="rootClass">
+    <aside class="left-side-bar__container" :class="rootClass">
         <Listbox
             v-model="selectedPage"
             :options="dashboardPages"
             option-label="label"
             @change="changeRoute()"
         />
+        <div class="flex flex-col space-y-2 mt-auto">
+            <Button class="action__button" label="Configuración" :pt="buttons_pt_property" />
+            <Button class="action__button" label="Cerrar Sesión" :pt="buttons_pt_property" />
+        </div>
     </aside>
 </template>
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+.left-side-bar__container
+     @apply flex flex-col
+     .action__button
+        background-color: var(--background-gray-variant)
+        color: var(--gray-text)
+        font-size: .9rem
+        &:hover
+            background-color: var(--primary-color)
+            color: var(--white-text)
+</style>
